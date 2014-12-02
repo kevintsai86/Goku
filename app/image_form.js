4e55bd7f97d2e793c4db86ffa2eb0b2d300c1edf
Kevin Tsai <visfarix@icloud.com>
2014-12-02 17:51:41 +0800

    Improved image_form UI
    Also made a test build for more interactive image_form UI

--- app/image_form.js
+++ app/image_form.js
@@ -1,45 +1,23 @@
 /* Javascript file for Image Control */
 //
+//Global variables
+//
+var imageID = 0; //for selecting which image to manipulate
 
 //
 //Functions for changing out pictures
 //
-//Reset function
+//Reset: Fade out function
 function resetBtns(delay, callback){
 	$('img').css({opacity: 0});
 	window.setTimeout(callback, delay);
 }
-//Display function
+//Display: Fade in function
 function dsplyImg(imgID){
 	$('img').css({display:'none'});
 	$('#imgDsply'+imgID).css({display:'inline-block'});
 	$('#imgDsply'+imgID).animate({opacity:1},600);
 }
-
-//Fade Functions *DEFUNCT
-/*function fadeLeft(){
-	$('img').animate({opacity:0},700,function(){
-		$('#imgDsply1').animate({opacity:1},700);
-	});
-}
-function fadeRight(){
-	$('img').animate({opacity:0},700,function(){
-		dsplyRight();
-		$('#imgDsply2').animate({opacity:1},700);
-	});
-}
-function fadeBoth(){
-	$('img').animate({opacity:0},700,function(){
-		dsplyBoth();
-		$('#imgDsply3').animate({opacity:1},700);
-	});	
-}
-function fadeNeither(){
-	$('img').animate({opacity:0},700,function(){
-		dsplyNeither();
-		$('#imgDsply4').animate({opacity:1},700);
-	});
-}*/
 
 //Slide functions
 function slideImg(imgID){
@@ -48,10 +26,10 @@ function slideImg(imgID){
 	$('img').css({display:'none'});
 	$('#imgDsply'+imgID).css({display:'inline-block'});
 	//trigger to start slide effect
-	$('#slideEvent').trigger('run', [imgID]);
+	$('#slideEvent').trigger('slideIn', [imgID]);
 }
-// slide event: slide function triggers the event to slide images into position
-$('#slideEvent').on('run', function(event, imgID){
+	// slide in event: slide function triggers the event to slide images into position
+$('#slideEvent').on('slideIn', function(event, imgID){
 	$('#imgDsply'+imgID).css({opacity:''});
 	setTimeout(function(){
 		$('#imgDsply'+imgID).toggleClass('slidePic'+imgID);
@@ -63,60 +41,49 @@ $('#slideEvent').on('run', function(event, imgID){
 //
 //reset button
 $('#resetBtn').click(function(){
-	resetBtns(700);
+	resetBtns(600);
 });
+
 //Display radio buttons **Note timing is off when you start hitting buttons in rapid succession
-$('#leftRad').click(function(){
-	resetBtns(700, function(){
+$('#1').click(function(){
+	resetBtns(600, function(){
 		dsplyImg(1);
 	});
 });
-$('#rightRad').click(function(){
-	resetBtns(700, function(){
+$('#2').click(function(){
+	resetBtns(600, function(){
 		dsplyImg(2);
 	});
 });
-$('#bothRad').click(function(){
-	resetBtns(700, function(){
+$('#3').click(function(){
+	resetBtns(600, function(){
 		dsplyImg(3);
 	});
 });
-$('#neitherRad').click(function(){
-	resetBtns(700, function(){
+$('#4').click(function(){
+	resetBtns(600, function(){
 		dsplyImg(4);
 	});
 });
-//Fade buttons *DEFUNCT
-/*$('#leftFade').click(function(){
-	fadeLeft();
-});
-$('#rightFade').click(function(){
-	$('img').promise().done(fadeRight());
-});
-$('#bothFade').click(function(){
-	$('img').promise().done(fadeBoth());
-});
-$('#neitherFade').click(function(){
-	$('img').promise().done(fadeNeither());
-});*/
+
 //Slide buttons
 $('#leftSlide').click(function(){
-	resetBtns(700, function(){
+	resetBtns(600, function(){
 		slideImg(1);
 	});
 });
 $('#rightSlide').click(function(){
-	resetBtns(700, function(){
+	resetBtns(600, function(){
 		slideImg(2);
 	});
 });
 $('#bothSlide').click(function(){
-	resetBtns(700, function(){
+	resetBtns(600, function(){
 		slideImg(3);
 	});
 });
 $('#neitherSlide').click(function(){
-	resetBtns(700, function(){
+	resetBtns(600, function(){
 		slideImg(4);
 	});
 });
---
+++ app/testCode.js
@@ -0,0 +1,105 @@
+/* Javascript file for Image Control */
+//
+//Global variables
+//
+var imageID = 'err'; //for selecting which image to manipulate
+
+//
+//Functions for changing out pictures
+//
+//Error: didn't pick a direction
+function noPicks(){
+	if(imageID == 'err'){
+		alert('No image selected');
+	}
+}
+//Reset: Fade out function
+function fadeOutImg(delay, callback){
+	$('img').css({opacity: 0});
+	window.setTimeout(callback, delay);
+}
+//Display: Fade in function
+function fadeInImg(imgID){
+	$('img').css({display:'none'});
+	$('#imgDsply'+imgID).css({display:'inline-block'});
+	$('#imgDsply'+imgID).animate({opacity:1},600);
+}
+
+//Slide functions
+function slideOutImg(imgID, delay, callback){
+	var slideway=0;
+	if(imgID==1)
+		slideway=2;
+	else if(imgID==2)
+		slideway=1;
+	else
+		slideway=imgID;
+	// console.log(imgID+' should slide in direction '+slideway);
+	$('#imgDsply'+imgID).toggleClass('slidePic'+slideway);
+	fadeOutImg();
+	window.setTimeout(callback, delay);
+}
+function slideInImg(imgID){
+	//positions the element off center
+	$('img').css({display:'none'});
+	$('#imgDsply'+imgID).toggleClass('slidePic'+imgID);
+	$('#imgDsply'+imgID).css({display:'inline-block'});
+	//trigger to start slide effect
+	$('#slideInEvent').trigger('slideIn', [imgID]);
+	console.log('slideInImg function complete');
+}
+	// slide in event: slide function triggers the event to slide images into position
+$('#slideInEvent').on('slideIn', function(event, imgID){
+	$('#imgDsply'+imgID).css({opacity:''});
+	setTimeout(function(){
+		$('#imgDsply'+imgID).toggleClass('slidePic'+imgID);
+	}, 650);
+	console.log('Slide In Event complete');
+});
+
+//
+//Click events for the Img Control Panel
+//
+//check radio button has been selected
+$('.btn').click(function(){
+	noPicks();
+});
+//reset button
+$('#resetBtn').click(function(){
+	$('img').css({opacity:'', display:''});
+});
+
+//Select Image
+$('.radioImgBtn').click(function(){
+	imageID = this.id;
+});
+
+//Fade In/out buttons **Note timing is off when you start hitting buttons in rapid succession
+$('#fadeBtn').click(function(){
+	fadeOutImg(650, function(){
+		fadeInImg(imageID);
+	});
+});
+$('#fadeInBtn').click(function(){
+	$('img').css({opacity: 0});
+	fadeInImg(imageID);
+});
+$('#fadeOutBtn').click(function(){
+	fadeOutImg();
+});
+
+//Slide In/out buttons
+$('#slideBtn').click(function(){
+	fadeOutImg(650, function(){
+		slideInImg(imageID);
+	});
+});
+//slide out is weird right now when you use it by itself
+$('#slideInBtn').click(function(){
+	slideInImg(imageID);
+});
+$('#slideOutBtn').click(function(){
+	slideOutImg(imageID,function(){
+		$('#imgDsply'+imageID).toggleClass('slidePic'+imageID);
+	});
+});
